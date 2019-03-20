755358aecc2c2a139baa82cc436b277770e29225
SystemGlitch <jeremy.la@outlook.fr>
2019-03-20 16:42:50 +0100

    Add File model

---
+++ src/main/java/fr/elyssif/client/gui/repository/FileRepository.java
@@ -0,0 +1,9 @@
+package fr.elyssif.client.gui.repository;
+
+import fr.elyssif.client.gui.model.File;
+
+public class FileRepository extends Repository<File> {
+
+	// TODO implement FileRepository
+
+}
